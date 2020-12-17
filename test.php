d91beffaf70cf7cf56a485e2538a04bad85cb2c2
JoshWachira <Joshua.wachira@strathmore.edu>
2020-12-17 09:13:23 +0300

    changes to temp and test

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
  <html>
- <head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script></head>
+ <head>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ </head>
     <body>
     Fill -ID,NAME,EMAIL_ID,PASSWORD,CREDITS,
-    <form action="test.php" method="post"> 
-
+    <form action="test.php" method="post">
 
 NAME: <input id="name" type="text" name="name"><br><br>
 PASSWORD: <input id="msg" type="text" name="msg"><br><br>
-
 
 <input type="submit" id="submit" name="submit">
 <button type="button" class="btn btn-primary" onclick="fun()">Add Record</button>
@@ -15,37 +15,37 @@ PASSWORD: <input id="msg" type="text" name="msg"><br><br>
 
 <script type="text/javascript">
     $("#submit").click(function() {
-            var name= $("#name").val();
-            var msg= $("#msg").val();
-            alert(msg);
-            $.post({
-                type: "POST",
-                url: "insert.php",
-                data: 'name=' + name+ '&msg=' + msg,
-                success: function(data) 
-                {
-                   alert(data);
-                }
-            });
+          var name= $("#name").val();
+          var msg= $("#msg").val();
+          alert(msg);
+          $.post({
+              type: "POST",
+              url: "insert.php",
+              data: 'name=' + name+ '&msg=' + msg,
+              success: function(data)
+              {
+                 alert(data);
+              }
+          });
 
 
-        });
+      });
 function fun()
 {
-    $.get('insert.php', {name : $("#name").val(),msg : $("#msg").val()}, function(data) 
-    {
-        //here you would write the "you ve been successfully subscribed" div
-        /*alert(data);*/
-        $("#name").val() == "";
-        $("#msg").val() == "";
+    $.get('insert.php', {name : $("#name").val(),msg : $("#msg").val()}, function(data)
+  {
+      //here you would write the "you ve been successfully subscribed" div
+      /*alert(data);*/
+      $("#name").val() == "";
+      $("#msg").val() == "";
 
-    });
+  });
 }
 
 </script>
 <?php
 /*include 'db.php';
-if (isset($_POST['submit'])) 
+if (isset($_POST['submit']))
 {
     $name =$_POST['name'];
     $msg =$_POST['msg'];
@@ -54,7 +54,7 @@ if (isset($_POST['submit']))
 
     $run = $db->query($insert);
 
-    if ($run) 
+    if ($run)
     {
         echo "true";
     }
@@ -68,4 +68,3 @@ if (isset($_POST['submit']))
 ?>
 </body>
 </html>
-
