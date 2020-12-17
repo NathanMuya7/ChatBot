59582c4d0500d137f67c1a9bc1879b4046389252
Cliff <cliff.wmosi@strathmore.edu>
2020-12-17 08:22:16 +0300

    Changes made

--- db.php
+++ db.php
@@ -1,17 +1,18 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "chat";
-
+ $username = "root";
+ $password = "";
+ $dbname = "chat";
 
 /*$conn = mysqli_connect($servername, $username, $password, $dbname);*/
 $db = new mysqli($servername, $username, $password, $dbname);
 
 function formatDate($date)
 {
+
 	return date('g:i:a',strtotime($date));
+
 }
 
 ?>
